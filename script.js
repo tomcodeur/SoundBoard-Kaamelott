59abbd7ac17d2f1ea5b37dd8400f16e77eac2ddb
Thomas <mercier.thomas@tomcodeur.com>
2021-03-23 18:12:01 +0100

    Projet SoundBoard Kaamelott

---
+++ script.js
@@ -0,0 +1,58 @@
+// Arthur
+
+let buttonArthur = document.getElementById('arthur');
+let audioArthur = document.getElementById('grandcornichon');
+
+buttonArthur.addEventListener('click', () => {  
+    audioArthur.play();
+}); 
+
+// Perceval
+
+let buttonPerceval = document.getElementById('perceval');
+let audioPerceval = document.getElementById('cpasfaux');
+
+buttonPerceval.addEventListener('click', () => {  
+    audioPerceval.play();
+}); 
+
+// Karadoc
+
+let buttonKaradoc = document.getElementById('karadoc');
+let audioKaradoc = document.getElementById('ccomplique');
+
+buttonKaradoc.addEventListener('click', () => {  
+    audioKaradoc.play();
+}); 
+
+// Kadoc
+
+let buttonKadoc = document.getElementById('kadoc');
+let audioKadoc = document.getElementById('caca');
+
+buttonKadoc.addEventListener('click', () => {  
+    audioKadoc.play();
+}); 
+
+
+// Léodagan
+
+let buttonLeodagan = document.getElementById('leodagan');
+let audioLeodagan = document.getElementById('merde');
+
+buttonLeodagan.addEventListener('click', () => {  
+    audioLeodagan.play();
+}); 
+
+
+// Guenièvre
+
+let buttonGuenievre = document.getElementById('guenievre');
+let audioGuenievre = document.getElementById('padutout');
+
+buttonGuenievre.addEventListener('click', () => {  
+    audioGuenievre.play();
+}); 
+
+
+
